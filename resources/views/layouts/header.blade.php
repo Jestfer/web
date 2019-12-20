0ba7a98a9012e8176d3523b7a6897a26ba8a2596
Jaime Sares <jaimesares@gmail.com>
2019-12-20 23:05:58 +0100

    wip

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,7 +5,7 @@
                 <div class="logo w-12 lg:w-1/5">
                     <a href="{{ route('home') }}"><img src="{{ asset('images/logo.png') }}" alt="{{ config('app.name') }}" class="w-16"></a>
                 </div>
-                <div class="lg:block w-12 lg:w-1/3 px-6 ml-auto">
+                <div class="lg:block w-12 lg:w-1/3 lg:px-6 ml-auto">
                     <div class="flex justify-end items-center text-gray-600">
                         <a class="block flex items-center hover:text-gray-700 mr-4" href="mailto:sukycms@gmail.com">
                             {{ __('web.contact') }}
