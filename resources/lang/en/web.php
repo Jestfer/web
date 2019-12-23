8afc3978efbaaea7ac1b1eee9d78cbeca15a452f
Josué Estévez Fernández <31468781+Jestfer@users.noreply.github.com>
2019-12-23 10:36:12 +0000

    Create web.php
    - web.php translation to EN

---
+++ resources/lang/en/web.php
@@ -0,0 +1,82 @@
+<?php
+
+return [
+
+    'slogan' => 'Animal Shelter Manager',
+    'description' => 'Free Animal Shelter Manager',
+
+    'what_is' => 'What is SukyCMS?',
+    'what_is_text' => 'SukyCMS is a free software manager for animal shelters. Create your website in seconds and start managing it.',
+    'email' => 'Email',
+    'contact' => 'Contact',
+
+    'shelters' => 'Shelters',
+    'animals_list' => 'List of animals',
+    'animal_file' => 'Animal file',
+
+    'features' => [
+        'title' => 'Features',
+
+        'easy' => [
+            'title' => 'Easy',
+            'description' => 'The project is designed so that people with little computing knowledge can use it. You just need to get used to the interface and start managing!',
+        ],
+        'custom' => [
+            'title' => 'Custom',
+            'description' => 'The project is created from scratch to help animal shelters with management. Unlike other projects, this one is thought to make it easier for shelters to perform their management tasks.',
+        ],
+        'free' => [
+            'title' => 'Free',
+            'description' => 'The project is and will always be free. Shelters won't have to pay to download or use it. This is our way to do our bits for the animal world.',
+        ],
+        'updates' => [
+            'title' => 'Updates',
+            'description' => 'The project features ongoing updates, improvements and fixes. Updates are automatic, so this is hassle-free for volunteers.',
+        ],
+        'security' => [
+            'title' => 'Security',
+            'description' => 'Security is very important for the project. We keep the software updated to the last version, make backups, etc.',
+        ],
+        'open' => [
+            'title' => 'Open source',
+            'description' => 'The project code is open source. This means that any user can read it, review it or even contribute actively to the development of the project.',
+        ],
+    ],
+
+    'other' => [
+        'admin' => [
+            'title' => 'Admin dashboard',
+            'description'  => '
+                <p class="mt-10 font-thin">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T).</p>
+                <p class="mt-5 font-thin">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T).</p>
+            ',
+        ],
+        'web' => [
+            'title' => 'Web',
+            'description' => '
+                <p class="mt-10 font-thin">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T).</p>
+                <p class="mt-5 font-thin">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T).</p>
+            ',
+        ],
+        'responsive' => [
+            'title' => 'Responsive',
+            'description' => '
+                <p class="mt-10 font-thin">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T).</p>
+                <p class="mt-5 font-thin">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T).</p>
+            ',
+        ]
+    ],
+
+    'footer' => [
+        'copyright' => 'Copyright &copy; :date - :name.',
+        'made_by' => 'Developed by :author con :icon',
+    ],
+
+    'actions' => [
+        'create_web' => 'Create website',
+        'more_about' => 'About',
+        'see_more' => 'See more',
+        'subscribe' => 'Subscribe',
+    ],
+
+];
